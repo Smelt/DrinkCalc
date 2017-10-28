8ccc67f5abf2abab0d36ff1252e0694cd01e0eb4
Smelt <shivamsatyarthi@gmail.com>
2017-10-28 14:15:01 -0400

    added Material Angular to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BuilderComponent } from './builder/builder.component';
 import { LandingComponent } from './landing/landing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+
 
 const appRoutes: Routes = [
   { path: 'build', component: BuilderComponent},
@@ -16,12 +20,15 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     BuilderComponent,
-    LandingComponent
+    LandingComponent,
+  
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
