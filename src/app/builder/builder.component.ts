da3bacb7f9505de0ff4cb85dd43712bf9f46cc62
Smelt <shivamsatyarthi@gmail.com>
2017-10-29 15:03:53 -0400

    material Loaded

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -12,4 +12,24 @@ export class BuilderComponent implements OnInit {
   ngOnInit() {
   }
 
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 1;
 }
+
+
