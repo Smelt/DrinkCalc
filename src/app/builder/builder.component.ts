eddf1459d90904eb6d53d8c2fbe51b226897c24e
Smelt <shivamsatyarthi@gmail.com>
2017-10-29 22:56:21 -0400

    changes that break string interprolation

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { User} from '../model/user.model'
+import {Drink} from '../model/drink.model';
 
 @Component({
   selector: 'app-builder',
@@ -7,29 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuilderComponent implements OnInit {
 
+  drinksArr: Drink[];
+  emptyDrink: Drink;
+  user: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.drinksArr = new Array();
+    this.emptyDrink = new Drink("Wine",12,1.0);
+    this.user = new User("Tom", "Smith", "male", 185);
   }
 
-  autoTicks = false;
-  disabled = false;
-  invert = false;
-  max = 100;
-  min = 0;
-  showTicks = false;
-  step = 1;
-  thumbLabel = false;
-  value = 0;
-  vertical = false;
+  onAddDrink(){
+    this.drinksArr.push(this.emptyDrink);
+    this.emptyDrink = new Drink("Wine",12,1.0);
+  }
 
-  get tickInterval(): number | 'auto' {
-    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  onResetDrink(){
+    this.emptyDrink = new Drink("Wine",12,1.0);
   }
-  set tickInterval(v) {
-    this._tickInterval = Number(v);
+
+   getBlue(){
+    return "green";
   }
-  private _tickInterval = 1;
+
+  static getCurrHour(){
+    const d: Date = new Date();
+    d.getHours();
+    return 3;
+  }
+
+  calculateBAC(){
+    let totalDrinksConsumed = 0;
+    for(let d of this.drinksArr){
+      totalDrinksConsumed += d.size;
+    }
+    let gramsAlcohol = 14 * totalDrinksConsumed;
+    let gramsBody = 454 * this.user.weight;
+    let rawBAC  = (gramsAlcohol)/(gramsBody * this.user.sexConstant);
+    let currHour = BuilderComponent.getCurrHour();
+  }
+  
 }
 
 
---
+++ src/app/model/drink.model.ts
@@ -0,0 +1,14 @@
+
+export class Drink {
+    public type: string;
+    public time: number;
+    public size: number;
+
+
+    constructor(type: string, time: number, size: number){
+        this.type = type;
+        this.time = time;
+        this.size = size;
+    }
+
+}
